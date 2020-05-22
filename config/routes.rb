065fe4ce2ea281bf49215cb15080d593deb742b0
Alex Platteeuw <aplatteeuw@outlook.fr>
2020-05-22 10:07:10 +0200

    added search bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :flats
+  get 'flat?query=[:keyword]', to: 'flats#query'
   root 'flats#index'
 end
